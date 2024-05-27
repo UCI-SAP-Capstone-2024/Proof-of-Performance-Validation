1e43f08f2a33228e9c1e7fbb1e13594458d9eb0f
Nike1421 <ommiemo54@gmail.com>
2024-05-26 19:01:10 -0700

    Update Utils

--- ui/deploy_demo/imageres.py
+++ ui/deploy_demo/imageres.py
@@ -14,26 +14,6 @@ def resize_with_padding(input_path, output_path, expected_size=(1024, 1024)):
     # Save the new image
     new_img.save(output_path)
 
-
-# def resize_and_pad_image(input_path, output_path, size=(1024, 1024)):
-#     image = Image.open(input_path)
-    
-#     # Resize image to fit within the specified size while maintaining aspect ratio
-#     image.resize(size, Image.LANCZOS)
-    
-#     # Create a new image with the specified size and white background
-#     new_image = Image.new("RGB", size, (255, 255, 255))
-    
-#     # Calculate position to paste the resized image onto the new image
-#     paste_x = (size[0] - image.size[0]) // 2
-#     paste_y = (size[1] - image.size[1]) // 2
-    
-#     # Paste the resized image onto the new image
-#     new_image.paste(image, (paste_x, paste_y))
-    
-#     # Save the new image
-#     new_image.save(output_path)
-
 def process_images(input_dir, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
